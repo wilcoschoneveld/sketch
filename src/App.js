9678eb8939f915115a64d2973f76fd88e34c7c09
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-02-02 16:01:23 +0100

    Working dynamic scene from blender

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
-import { OrbitControls } from '@react-three/drei'
-import { Suspense, useRef, useState } from 'react'
-import { Canvas, useFrame, useLoader } from 'react-three-fiber'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js'
-
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
-
-function Test() {
-  const materials = useLoader(MTLLoader, 'assets/test2.mtl')
-  const object = useLoader(OBJLoader, 'assets/test2.obj', loader => {
-    materials.preload()
-    loader.setMaterials(materials)
-  });
-  return <primitive object={object} />
-}
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Suspense } from 'react'
+import { Canvas } from 'react-three-fiber'
+import Model from './Model'
 
 function App() {
   return (
     <Canvas>
+      <PerspectiveCamera makeDefault position={[1, 1, 1]} />
       <OrbitControls />
-      <ambientLight />
+      <ambientLight intensity={0.2} />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
       <Suspense fallback={null}>
-        <Test />
+        <Model />
       </Suspense>
     </Canvas>
   );
