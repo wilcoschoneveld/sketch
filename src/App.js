a87fb12742fdb5179b1a11ca9b1bfac990938756
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-02-03 20:38:43 +0100

    First attempt at throwing a box with manual velocity calculation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Physics } from '@react-three/cannon';
 import { OrbitControls, PerspectiveCamera, Sky } from '@react-three/drei';
 import { DefaultXRControllers, VRCanvas } from '@react-three/xr';
 import { Suspense } from 'react';
@@ -10,13 +11,15 @@ function App() {
       <Sky />
       <DefaultXRControllers />
       <PerspectiveCamera makeDefault position={[0, 1.5, 0]} />
-      <OrbitControls target={[0, 0, -10]} />
+      {/* <OrbitControls target={[0, 0, -10]} /> */}
       <ambientLight intensity={0.2} />
       <pointLight position={[10, 10, 10]} />
       <Suspense fallback={null}>
         <Model position={[2, 0, -5]} />
       </Suspense>
-      <Game />
+      <Physics>
+        <Game />
+      </Physics>
     </VRCanvas>
   );
 }
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,6 @@
 import { Physics, useBox, usePlane } from '@react-three/cannon'
 import { useXREvent } from '@react-three/xr'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import * as THREE from "three"
 
 function Plane(props) {
@@ -24,6 +24,53 @@ function Cube(props) {
 
 export default function Game() {
     const [cubes, setCubes] = useState([]);
+    const [state, setState] = useState({ value: "idle" });
+    const [ref, api] = useBox(() => ({ mass: 1, args: [0.5, 0.5, 0.5], position: [0, 0, -20] }));
+
+    const prevPosition = useRef();
+    const speed = useRef();
+
+    const onDown = (event) => {
+        console.log('down', event);
+        const { x, y, z } = event.intersections[0].point;
+        setState("dragging");
+        api.position.set(x, y, z);
+        prevPosition.current = event.intersections[0].point.clone();
+    }
+
+    const onMove = (event) => {
+        // console.log('move', event);
+        if (state === "dragging") {
+            // console.log('yes');
+            const { x, y, z } = event.intersections[0].point;
+            api.position.set(x, y, z);
+            api.velocity.set(0, 0, 0);
+
+            const currentPosition = event.intersections[0].point;
+            if (prevPosition.current) {
+                if (!speed.current) {
+                    speed.current = new THREE.Vector3();
+                }
+
+                const newSpeed = currentPosition.clone().sub(prevPosition.current).multiplyScalar(60);
+
+                speed.current.multiplyScalar(0.9).add(newSpeed.multiplyScalar(0.1));
+                // console.log(speed.current.x);
+            }
+            prevPosition.current = currentPosition.clone();
+            // console.log('hey');
+        }
+    }
+
+    const onUp = (event) => {
+        console.log('up');
+        setState("idle");
+        const { x, y, z } = event.intersections[0].point;
+        // api.position.set(x, y, z);
+        console.log(x, y, z);
+
+        api.velocity.set(speed.current.x, speed.current.y, speed.current.z);
+    }
 
     const onClick = (event) => {
         const { x, y, z } = event.intersections[0].point;
@@ -46,16 +93,20 @@ export default function Game() {
     })
 
     return (
-        <Physics>
-            <mesh position={[0, 5, -10]} onClick={onClick}>
+        <>
+            <mesh position={[0, 5, -10]} onPointerDown={onDown} onPointerMove={onMove} onPointerUp={onUp}>
                 <planeBufferGeometry attach="geometry" args={[10, 10]} />
                 <meshBasicMaterial attach="material" visible={false} />
             </mesh>
             <Plane />
             <Cube position={[0, 2, -10]} />
+            <mesh ref={ref}>
+                <boxBufferGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             {cubes.map((cube, i) =>
                 <Cube key={i} size={cube.small ? [0.1, 0.1, 0.1] : [1, 1, 1]} position={cube.position} />
             )}
-        </Physics>
+        </>
     )
 }
